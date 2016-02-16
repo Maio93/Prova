ddcf12e114ea8a6cda203f12701afc4b7fafa491
Maio93 <maio.cocchi@hotmail.it>
2016-02-16 11:44:45 +0100

    Inserimento Prova

---
+++ Progetto/simple-app/src/main/java/SimpleApp.scala
@@ -0,0 +1,16 @@
+/* SimpleApp.scala */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "/home/fabio/Scaricati/itwiki-20150121-wikipedia-links.ttl.gz" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+  }
+}
